15b5d4a8afd779a184bfd067006960f1de2e6002
Lorrainnn <jixual3@uci.edu>
2026-09-09 15:57:21 -0700

    combine on ncc/l2 functiion

--- 1/process.py
+++ 1/process.py
@@ -46,38 +46,6 @@ def croppping(im, fraction=0.1):
     ]
 
 
-#The easiest way to align the parts is to exhaustively search over a window of possible displacements (say [-15,15] pixels)
-def align_single_l2(mov, ref, search_radius=15):
-    best_score = float("inf")
-    best_shift = (0, 0)
-
-    reference_crop = croppping(ref)
-
-    for dy in range(-search_radius, search_radius + 1):
-        for dx in range(-search_radius, search_radius + 1):
-
-            shifted = np.roll(
-                mov,
-                shift=(dy, dx),
-                axis=(0, 1)
-            )
-
-            shifted_crop = croppping(shifted)
-
-            score = l2_score(
-                reference_crop,
-                shifted_crop
-            )
-
-            #update to best score
-            #so basically l2 should be expected to as small as possible
-
-            if score < best_score:
-                best_score = score
-                best_shift = (dy, dx)
-
-    return best_shift
-
 
 #this one should be as high as possible (close to 1)
 def ncc_score(im1, im2):
@@ -97,8 +65,15 @@ def ncc_score(im1, im2):
 
     return np.dot(a, b) / denominator
 
-def align_single_ncc(moving, reference, search_radius=15):
-    best_score = -float("inf")
+def align_single(moving, reference, style, search_radius=15):
+    if style == "ncc":
+        best_score = -float("inf")
+    elif style == "l2":
+        best_score = float("inf")
+    else:
+        raise ValueError("Unknown style")
+    
+    #init
     best_shift = (0, 0)
 
     for dy in range(-search_radius, search_radius + 1):
@@ -109,17 +84,30 @@ def align_single_ncc(moving, reference, search_radius=15):
                 shift=(dy, dx),
                 axis=(0, 1)
             )
-
-            score = ncc_score(
-                croppping(shifted),
-                croppping(reference)
-            )
-
-            if score > best_score:
-                best_score = score
-                best_shift = (dy, dx)
+            #we expect ncc close to 1
+            if style == "ncc":
+                score = ncc_score(
+                    croppping(shifted),
+                    croppping(reference)
+                )
+                if score > best_score:
+                    best_score = score
+                    best_shift = (dy, dx)
+            #as small as possible
+            elif style == "l2":
+                score = l2_score(
+                    croppping(shifted),
+                    croppping(reference)
+                )
+                if score < best_score:
+                    best_score = score
+                    best_shift = (dy, dx)
 
     return best_shift
+
+#The easiest way to align the parts is to exhaustively search over a window of possible displacements (say [-15,15] pixels)
+#def align_single_l2(mov, ref, search_radius=15):
+
 
 def run_single_scale(image_path):
     im = skio.imread(image_path)
@@ -128,8 +116,8 @@ def run_single_scale(image_path):
     B, G, R = split_color_channel(im)
 
     # L2
-    g_shift_l2 = align_single_l2(G, B)
-    r_shift_l2 = align_single_l2(R, B)
+    g_shift_l2 = align_single(G, B, style="l2")
+    r_shift_l2 = align_single(R, B, style="l2")
 
     G_l2 = np.roll(G, g_shift_l2, axis=(0, 1))
     R_l2 = np.roll(R, r_shift_l2, axis=(0, 1))
@@ -143,8 +131,8 @@ def run_single_scale(image_path):
 
 
     # NCC
-    g_shift_ncc = align_single_ncc(G, B)
-    r_shift_ncc = align_single_ncc(R, B)
+    g_shift_ncc = align_single(G, B, style="ncc")
+    r_shift_ncc = align_single(R, B, style="ncc")
 
     G_ncc = np.roll(G, g_shift_ncc, axis=(0, 1))
     R_ncc = np.roll(R, r_shift_ncc, axis=(0, 1))
@@ -168,6 +156,59 @@ def run_single_scale(image_path):
     return result_l2, result_ncc
 
 
+#p2
+
+def half_size(im):
+    return resize(im, (
+        im.shape[0] // 2,
+        im.shape[1] // 2
+    ), anti_aliasing=True)
+
+
+def search_shift_ncc(
+    moving,
+    reference,
+    center=(0, 0),
+    radius=15
+):
+    center_dy, center_dx = center
+
+    best_score = -float("inf")
+    best_shift = center
+
+    reference_crop = croppping(reference)
+
+    for dy in range(
+        center_dy - radius,
+        center_dy + radius + 1
+    ):
+        for dx in range(
+            center_dx - radius,
+            center_dx + radius + 1
+        ):
+
+            shifted = np.roll(
+                moving,
+                shift=(dy, dx),
+                axis=(0, 1)
+            )
+
+            shifted_crop = croppping(shifted)
+
+            score = ncc_score(
+                shifted_crop,
+                reference_crop
+            )
+
+            if score > best_score:
+                best_score = score
+                best_shift = (dy, dx)
+
+    return best_shift
+
+
+
+
 #generate results for jpg with single scale alignment
 
 l2_result, ncc_result  = run_single_scale("1/data/tobolsk.jpg")
@@ -184,3 +225,5 @@ plt.title("tobolsk - NCC")
 plt.axis("off")
 plt.savefig("1/output/ncc_tobolsk.jpg")
 plt.show()
+
+
