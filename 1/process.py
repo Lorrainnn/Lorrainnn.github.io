188565b46c0b3bcbdf395c20d450ef19a00be948
Lorrainnn <jixual3@uci.edu>
2026-09-09 17:03:24 -0700

    p1 check. start on better feature on emir

--- 1/process.py
+++ 1/process.py
@@ -166,29 +166,122 @@ def half_size(im):
     ), anti_aliasing=True)
 
 
-#In this case, you will need to implement a faster search procedure such as an image pyramid. An image pyramid represents the image at multiple scales (usually scaled by a factor of 2) and the processing is done sequentially starting from the coarsest scale (smallest image) and going down the pyramid, updating your estimate as you go. It is very easy to implement by adding recursive calls to your original single-scale implementation. 
+# In this case, you will need to implement a faster search procedure such as an image pyramid. An image pyramid represents the image at multiple scales (usually scaled by a factor of 2) and the processing is done sequentially starting from the coarsest scale (smallest image) and going down the pyramid, updating your estimate as you go. It is very easy to implement by adding recursive calls to your original single-scale implementation. 
 # You should implement the pyramid functionality yourself using appropriate downsampling techniques.
-def align_pyramid(mov, ref, style, min_size=500, coarse_radius=15, refine_radius=3):
+def align_pyramid(mov, ref, style, min_size=500, search_radius=15, refine_radius=3):
     h, w = ref.shape
 
+    #reach the smallest size, adapt single scale alignment
+    #end of the recursion call
+    if max(h, w)<= min_size:
+        return align_single(mov, ref, style=style, center=(0, 0), search_radius=search_radius)
 
-    if max(h, w) 
+    #downsize
+    mov_small = half_size(mov)
+    ref_small = half_size(ref)
+
+    #recusive call to align_pyramid
+    small_shift = align_pyramid(
+        mov_small,
+        ref_small,
+        style=style,
+        min_size=min_size,
+        search_radius=search_radius,
+        refine_radius=refine_radius
+    )
+
+    # upsize the shift
+    predicted_shift = (
+        2 * small_shift[0],
+        2 * small_shift[1]
+    )
+  
+    return align_single(
+        mov,
+        ref,
+        style=style,
+        center=predicted_shift,
+        search_radius=refine_radius
+    )
+
+
 
 #generate results for jpg with single scale alignment
+#l2_result, ncc_result  = run_single_scale("1/data/tobolsk.jpg")
+#plt.figure()
+#plt.imshow(np.clip(l2_result, 0, 1))
+#plt.title("tobolsk - L2")
+#plt.axis("off")
+#plt.savefig("1/output/l2_tobolsk.jpg")
+#plt.show()
 
-l2_result, ncc_result  = run_single_scale("1/data/tobolsk.jpg")
+#plt.figure()
+#plt.imshow(np.clip(ncc_result, 0, 1))
+#plt.title("tobolsk - NCC")
+#plt.axis("off")
+#plt.savefig("1/output/ncc_tobolsk.jpg")
+#plt.show()
+
+
+
+
+#this one use for p2 tiff pyramid scale output visualization
+def run_pyramid_scale(image_path):
+    im = skio.imread(image_path)
+    im = img_as_float32(im)
+
+    B, G, R = split_color_channel(im)
+
+    # L2
+    g_shift_l2 = align_pyramid(G, B, style="l2")
+    r_shift_l2 = align_pyramid(R, B, style="l2")
+
+    G_l2 = np.roll(G, g_shift_l2, axis=(0, 1))
+    R_l2 = np.roll(R, r_shift_l2, axis=(0, 1))
+
+    result_l2 = np.dstack([
+        R_l2,
+        G_l2,
+        B
+    ])
+
+
+    # NCC
+    g_shift_ncc = align_pyramid(G, B, style="ncc")
+    r_shift_ncc = align_pyramid(R, B, style="ncc")
+
+    G_ncc = np.roll(G, g_shift_ncc, axis=(0, 1))
+    R_ncc = np.roll(R, r_shift_ncc, axis=(0, 1))
+
+    result_ncc = np.dstack([
+        R_ncc,
+        G_ncc,
+        B
+    ])
+
+    print("\n", image_path)
+
+    print("L2:")
+    print("  G (x, y):", (g_shift_l2[1], g_shift_l2[0]))
+    print("  R (x, y):", (r_shift_l2[1], r_shift_l2[0]))
+
+    print("NCC:")
+    print("  G (x, y):", (g_shift_ncc[1], g_shift_ncc[0]))
+    print("  R (x, y):", (r_shift_ncc[1], r_shift_ncc[0]))
+
+    return result_l2, result_ncc
+
+l2_result, ncc_result  = run_pyramid_scale("1/data/emir.tif")
 plt.figure()
 plt.imshow(np.clip(l2_result, 0, 1))
-plt.title("tobolsk - L2")
+plt.title("emir - Pyramid L2")
 plt.axis("off")
-plt.savefig("1/output/l2_tobolsk.jpg")
+plt.savefig("1/output/l2_pyramid_emir_500.jpg")
 plt.show()
 
 plt.figure()
 plt.imshow(np.clip(ncc_result, 0, 1))
-plt.title("tobolsk - NCC")
+plt.title("emir - Pyramid NCC")
 plt.axis("off")
-plt.savefig("1/output/ncc_tobolsk.jpg")
+plt.savefig("1/output/ncc_pyramid_emir_500.jpg")
 plt.show()
-
-
